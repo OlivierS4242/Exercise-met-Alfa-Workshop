973c2c8e72a9306b5e9c87f22eb23975be01ddb7
OlivierS4242 <otmschutte@me.com>
2023-02-10 15:17:16 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-Console.WriteLine("How many seconds");
-int seconds = Convert.ToInt32(Console.ReadLine());
-float hours = seconds / 3600;
-double frac = hours % 1;
-float min = (float)frac * 60;
-double frac2 = min % 1;
-int sec = (int)frac2 * 60;
-Console.WriteLine("Hours: " + hours);
-Console.WriteLine("Minutes: " + min);
-Console.WriteLine("Seconds left: "+sec);
-
+﻿
